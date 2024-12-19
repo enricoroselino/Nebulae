405905f58773f01cfc10e3ea0cfb3c8c636d61bf
enricoroselino <enricoroselino@gmail.com>
2024-12-19 09:52:18 +0700

    feat(users): implemented Get Users and by User Id

---
+++ API.Modules.Identity/Features/GetUsers/GetUsersEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using Ardalis.Result.AspNetCore;
+using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace API.Modules.Identity.Features.GetUsers;
+
+public class GetUsersEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup(ModuleConfig.IamGroup);
+
+        group.MapGet("/users", async (
+            ISender mediator,
+            CancellationToken cancellationToken) =>
+        {
+            var query = new GetUsersQuery();
+            var result = await mediator.Send(query, cancellationToken);
+            return result.ToMinimalApiResult();
+        });
+    }
+}
---
+++ API.Modules.Identity/Features/GetUsers/GetUsersQuery.cs
@@ -0,0 +1,27 @@
+﻿using API.Modules.Identity.Dtos;
+using API.Shared.Models.CQRS;
+using Ardalis.Result;
+
+namespace API.Modules.Identity.Features.GetUsers;
+
+public record GetUsersQuery() : IQuery<Result<List<UserDto>>>;
+
+public class GetUsersQueryHandler : IQueryHandler<GetUsersQuery, Result<List<UserDto>>>
+{
+    private readonly AppIdentityDbContext _dbContext;
+
+    public GetUsersQueryHandler(AppIdentityDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Result<List<UserDto>>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+    {
+        var users = await _dbContext.Users
+            .AsNoTracking()
+            .Select(c => UserDto.Create(c))
+            .ToListAsync(cancellationToken);
+
+        return Result.Success(users);
+    }
+}
